f3eca60c7b0cc8e15657b49c3dc1bb9a8eb23e53
Whiskee <250067450@qq.com>
2025-05-21 16:54:32 +0800

    feat(ble): add filters to BleSnRule for enhanced serialization
    - Introduced a new `filters` property in `BleSnRule` to support a list of filter strings.
    - Updated serialization methods to include the `filters` property, ensuring it defaults to an empty list if not provided.

--- lib/models/ble_sn_rule.g.dart
+++ lib/models/ble_sn_rule.g.dart
@@ -10,10 +10,15 @@ BleSnRule _$BleSnRuleFromJson(Map<String, dynamic> json) => BleSnRule(
       byteLength: (json['byteLength'] as num?)?.toInt() ?? 0,
       startSubIndex: (json['startSubIndex'] as num?)?.toInt() ?? 0,
       replaceRex: json['replaceRex'] as String? ?? "[\\x{00}-\\x{1F}\\x{7F}]",
+      filters: (json['filters'] as List<dynamic>?)
+              ?.map((e) => e as String)
+              .toList() ??
+          const [],
     );
 
 Map<String, dynamic> _$BleSnRuleToJson(BleSnRule instance) => <String, dynamic>{
       'byteLength': instance.byteLength,
       'startSubIndex': instance.startSubIndex,
       'replaceRex': instance.replaceRex,
+      'filters': instance.filters,
     };
