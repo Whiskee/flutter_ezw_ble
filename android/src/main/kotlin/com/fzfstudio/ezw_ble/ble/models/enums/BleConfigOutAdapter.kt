b85abc3af1d1fb6c4dca842b9e7f52cf84f0e13d
Whiskee <250067450@qq.com>
2025-05-22 14:30:06 +0800

    refactor(ble): change BleConfigOutAdapter to serialize BleConfig
    The adapter was previously serializing BleConnectState, but it has been updated to serialize BleConfig instead. This change aligns the serializer with the correct data model.

--- android/src/main/kotlin/com/fzfstudio/ezw_ble/ble/models/enums/BleConfigOutAdapter.kt
+++ android/src/main/kotlin/com/fzfstudio/ezw_ble/ble/models/enums/BleConfigOutAdapter.kt
@@ -1,14 +1,15 @@
 package com.fzfstudio.ezw_ble.ble.models.enums
 
+import com.fzfstudio.ezw_ble.ble.models.BleConfig
 import com.google.gson.JsonElement
 import com.google.gson.JsonPrimitive
 import com.google.gson.JsonSerializationContext
 import com.google.gson.JsonSerializer
 import java.lang.reflect.Type
 
-class BleConfigOutAdapter: JsonSerializer<BleConnectState> {
+class BleConfigOutAdapter: JsonSerializer<BleConfig> {
     override fun serialize(
-        src: BleConnectState?,
+        src: BleConfig?,
         typeOfSrc: Type?,
         context: JsonSerializationContext?
     ): JsonElement? = JsonPrimitive(src?.name)
